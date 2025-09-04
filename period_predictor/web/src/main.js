038860fcd82e7cc7e8e3bcce9c1d82755edda69e
Adam Morrison <morrison.adam.s@gmail.com>
2025-09-04 13:56:27 -0500

    fix: remove daygrid css import

--- period_predictor/web/src/main.js
+++ period_predictor/web/src/main.js
@@ -2,6 +2,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './styles/sidebar.css'
 import './styles/fullcalendar-core.css'
-import '@fullcalendar/daygrid/index.css'
 
 createApp(App).mount('#app')
